81b126a5ae7facbcbb9d7ae0e73359bd7fdd8029
nathan <nabarca@chicagotechacademy.org>
2014-01-14 13:12:00 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apperino::Application.config.secret_key_base = '686c9d44666c3eb0a054c08b70011bf1847538a9e86ad14eb9cc4b3aadf83140ef84897f07e108b2f295d7a00779291cba567dcfe31d66c4c6ea75fe2c7d3e4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apperino::Application.config.session_store :cookie_store, key: '_apperino_session'
